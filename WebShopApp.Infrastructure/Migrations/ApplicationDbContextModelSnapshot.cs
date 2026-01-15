9d42a1b637269e0ffa1f796884e39d8dd0324c02
Niki Yotov <nikiyotov27@Nikis-MacBook-Air.local>
2026-01-15 12:47:21 +0200

    part 5

--- WebShopApp.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
+++ WebShopApp.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,10 +17,7 @@ namespace WebShopApp.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.22")
-                .HasAnnotation("Proxies:ChangeTracking", false)
-                .HasAnnotation("Proxies:CheckEquality", false)
-                .HasAnnotation("Proxies:LazyLoading", true)
+                .HasAnnotation("ProductVersion", "8.0.18")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
